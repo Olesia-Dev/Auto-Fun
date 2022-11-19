5592238e4b17fc230875f0fcfc34521a88aa78a1
Olesia Zaiats <olesia.zayats@gmail.com>
2022-11-18 21:00:38 -0600

    LinkedList, printing last element

--- src/com/fun/auto/Main.java
+++ src/com/fun/auto/Main.java
@@ -4,6 +4,7 @@ import com.fun.auto.model.Car;
 import com.fun.auto.model.Truck;
 import com.fun.auto.service.DataGenerator;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
@@ -15,11 +16,21 @@ public class Main {
         }
 
         List<Truck> trucks = DataGenerator.generateTrucks();
+//        trucks.get(trucks.size() - 1); Get last element
         for (Truck truck : trucks) {
             if (truck.getMaxSpeed() > 130) {
                 System.out.println(truck);
             }
         }
+
+        Main.printLastFromLinkedList();
+    }
+
+    private static void printLastFromLinkedList() {
+        LinkedList<String> strings = new LinkedList<>();
+        strings.add("first");
+        strings.add("second");
+        System.out.println(strings.getLast());
     }
 
 }
