ee6c1b6c124ef3eb36d9e6e5c850c8677962d63a
Olesia Zaiats <olesia.zayats@gmail.com>
2022-11-29 12:09:33 -0600

    Console car creation

--- src/com/fun/auto/ConsoleApp.java
+++ src/com/fun/auto/ConsoleApp.java
@@ -9,9 +9,19 @@ public class ConsoleApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Car Creator console application!");
         Scanner carScanner = new Scanner(System.in);
-        Car car = new Car();
+        Car car = new Car( );
         System.out.println("Please enter number of wheels:");
         car.setNumberOfWheels(Integer.parseInt(carScanner.nextLine()));
+        System.out.println("Please enter max speed:");
+        car.setMaxSpeed(Integer.parseInt(carScanner.nextLine()));
+        System.out.println("Please enter color:");
+        car.setColor(carScanner.nextLine());
+        System.out.println("Please enter number of seats:");
+        car.setNumberOfSeats(Integer.parseInt(carScanner.nextLine()));
+        System.out.println("Please enter automatic gear:");
+        car.setAutomaticGear(Boolean.parseBoolean(carScanner.nextLine()));
+        System.out.println("Please enter climate control");
+        car.setClimateControl(Boolean.parseBoolean(carScanner.nextLine()));
 
         // TODO add all car's parameters
 
