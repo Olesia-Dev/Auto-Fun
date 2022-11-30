eb413a14e31e1f3bb695bc92300ec4707e216648
Olesia Zaiats <olesia.zayats@gmail.com>
2022-11-29 22:09:22 -0600

    Create file and write into file

--- src/com/fun/auto/ConsoleApp.java
+++ src/com/fun/auto/ConsoleApp.java
@@ -2,6 +2,9 @@ package com.fun.auto;
 
 import com.fun.auto.model.Car;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class ConsoleApp {
@@ -9,23 +12,44 @@ public class ConsoleApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Car Creator console application!");
         Scanner carScanner = new Scanner(System.in);
-        Car car = new Car( );
-        System.out.println("Please enter number of wheels:");
-        car.setNumberOfWheels(Integer.parseInt(carScanner.nextLine()));
-        System.out.println("Please enter max speed:");
-        car.setMaxSpeed(Integer.parseInt(carScanner.nextLine()));
-        System.out.println("Please enter color:");
-        car.setColor(carScanner.nextLine());
-        System.out.println("Please enter number of seats:");
-        car.setNumberOfSeats(Integer.parseInt(carScanner.nextLine()));
-        System.out.println("Please enter automatic gear:");
-        car.setAutomaticGear(Boolean.parseBoolean(carScanner.nextLine()));
-        System.out.println("Please enter climate control");
-        car.setClimateControl(Boolean.parseBoolean(carScanner.nextLine()));
-
-        // TODO add all car's parameters
+        Car car = buildCarInConsole(carScanner);
 
         System.out.println("You have created new car: " + car);
+        writeCarIntoFile(car);
+    }
+
+    private static Car buildCarInConsole(Scanner scanner) {
+        Car car = new Car();
+        System.out.println("Please enter number of wheels:");
+        car.setNumberOfWheels(Integer.parseInt(scanner.nextLine()));
+        System.out.println("Please enter max speed:");
+        car.setMaxSpeed(Integer.parseInt(scanner.nextLine()));
+        System.out.println("Please enter color:");
+        car.setColor(scanner.nextLine());
+        System.out.println("Please enter number of seats:");
+        car.setNumberOfSeats(Integer.parseInt(scanner.nextLine()));
+        System.out.println("Please enter automatic gear:");
+        car.setAutomaticGear(Boolean.parseBoolean(scanner.nextLine()));
+        System.out.println("Please enter climate control");
+        car.setClimateControl(Boolean.parseBoolean(scanner.nextLine()));
+        return car;
+    }
+
+    private static void writeCarIntoFile(Car car) {
+        File carFile = new File("car.txt");
+        try {
+            if (carFile.createNewFile()) {
+                System.out.println("File created: " + carFile.getAbsolutePath());
+            } else {
+                System.out.println("File already exists.");
+            }
+            FileWriter carWriter = new FileWriter(carFile);
+            carWriter.write(car.toString());
+            carWriter.close();
+        } catch (IOException e) {
+            System.out.println("File error!");
+            e.printStackTrace();
+        }
     }
 
 }
